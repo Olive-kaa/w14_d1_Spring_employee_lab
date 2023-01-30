e487fd8abf6768c4251e1f507c4773ae191f9d6e
Olive-kaa <omkupinska@gmail.com>
2023-01-30 15:09:41 +0000

    uploading the lab

---
+++ src/test/java/com/codeclan/example/employeeservice/EmployeeserviceApplicationTests.java
@@ -0,0 +1,25 @@
+package com.codeclan.example.employeeservice;
+
+import com.codeclan.example.employeeservice.models.Employee;
+import com.codeclan.example.employeeservice.repositories.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeserviceApplicationTests {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee(){
+		Employee jack = new Employee("Jack Smith", 25, 1234, "jack@gmail.com");
+		employeeRepository.save(jack);
+	}
+
+}
